cb5ef8ebe85656a159fc6fb61e3de418d69f0196
thexro <ravyyadav97@gmail.com>
2024-10-09 00:21:04 +0530

    tool section done ig?

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -13,39 +13,55 @@ function Tools() {
       scrollTrigger: {
         toggleActions: "play none none none",
         trigger: ".tool",
-        start: "40% 100%",
-        end: "70% 80% ",
+        start: "40% 90%",
+        end: "80% 70% ",
         scrub: true,
 
         // markers: true,
       },
     });
     t1.from([".tool1, .tool2, .tool3, .tool4, .tool5"], {
-      duration: 0.3,
+      duration: 1,
       //   opacity: 0,
       //   delay: 0.4,
       scrub: "true",
-      translateY: "150",
-      stagger: 0.05,
-      ease: "power1.inOut",
+      translateX: "120",
+      stagger: 0.2,
+      ease: "power1.Out",
     });
   });
   return (
-    <div className="mx-[5vw] md:mx-[10vw] my-[1vw] md:flex flex-col relative h-screen ">
+    <div className="mx-[5vw] md:mx-[10vw] my-[1vw] md:flex flex-col relative overflow-hidden">
       <div className="flex flex-col ">
-        <div className="mb-10 tool h-[11vw] w-[18 overflow-hidden flex bf ">
-          <span className="text-[10vw] inline-block y tool1 ">T</span>
-          <span className="text-[10vw] inline-block y tool2 ">o</span>
-          <span className="text-[10vw] inline-block y tool3 ">o</span>
-          <span className="text-[10vw] inline-block y tool4 ">l</span>
-          <span className="text-[10vw] inline-block y tool5 ">s</span>
+        <div className="mb-10 tool h-[11vw] overflow-hidden flex bf ">
+          <div className="z-2 overflow-hidden">
+            <span className="text-[10vw] inline-block y tool1 z-1">T</span>
+          </div>
+          <div className="z-2 overflow-hidden">
+            <span className="text-[10vw] inline-block y tool2 ">o</span>
+          </div>
+          <div className="z-2 overflow-hidden">
+            <span className="text-[10vw] inline-block y tool3 ">o</span>
+          </div>
+          <div className="z-2 overflow-hidden">
+            <span className="text-[10vw] inline-block y tool4 ">l</span>
+          </div>
+          <div className="z-2 overflow-hidden">
+            <span className="text-[10vw] inline-block y tool5 ">s</span>
+          </div>
         </div>
       </div>
-      <div className="md:flex gap-2">
-        <div className="my-4 w-full">
-          {/* <div className="text-2xl md:text-[3vw] mb-2">Javascript </div> */}
-          <Hover front='JavaScript' styles="text-2xl md:text-[3vw] mb-2 text-white w-full"  >Adding interactivity and enhancing user experience.</Hover>
-        </div>
+      <div className="my-4 md:flex md:gap-8 flex gap-5  flex-col ">
+        {/* <div className="text-2xl md:text-[3vw] mb-2">Javascript </div> */}
+        <Hover front="JavaScript;">
+          Adding interactivity and enhancing user experience.
+        </Hover>
+        <Hover front="<React.js/>">Crafting dynamic user experiences</Hover>
+        <Hover front="/Node.js">Building server-side applications.</Hover>
+        <Hover front="Express.js">Creating robust APIs.</Hover>
+        <Hover front="MongoDB">
+          Designing aesthetically pleasing interfaces.
+        </Hover>
       </div>
     </div>
   );
