659af9e97d8831f60d4794d3805a8f6b5be9ff3f
thexro <ravyyadav97@gmail.com>
2024-10-09 00:19:56 +0530

    made some minor changes

--- src/components/Hover.jsx
+++ src/components/Hover.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
 
-function Hover({ front,children, styles, childrenStyles}) {
+function Hover({ front, children }) {
   return (
-    <div>
-      <div className="">
-        <div className="group relative font-black">
-          <h1 className={`duration-150 group-hover:opacity-0 text-black   ${styles}`}>{front}</h1>
-          <div class="absolute top-0 flex scale-y-0 items-center justify-center bg-[#fff] font-black duration-500 ease-in-out group-hover:scale-y-100">
-            <Marquee
-              className={`opacity-0 duration-500 group-hover:text-[#111] group-hover:opacity-100 ${childrenStyles}`}
-            >
-              {children} <span className="mx-10"></span>
-              {children}<span className="mx-10"></span>
-              {children}<span className="mx-10"></span>
-              {children}<span className="mx-10"></span>
-
-            </Marquee>
-          </div>
-        </div>
+    <div className="group relative font-black w-full"> {/* Use w-full here */}
+      <div className="flex justify-between w-full items-center">
+        <h1 className="duration-150 text-white text-xl "  > <span> --- </span> {front}</h1>
+        <div className="hidden md:inline-block text-center">+++</div>
       </div>
+      <div className="absolute top-0 scale-y-0 items-center justify-center bg-white font-black duration-500 ease-in-out group-hover:scale-y-100 hidden md:block w-full">
+        <Marquee className="duration-500 text-[#111] text-xl opacity-0 group-hover:opacity-100">
+          {children} <span className="mx-10"></span>
+          {children}
+          <span className="mx-10"></span>
+          {children}
+          <span className="mx-10"></span>
+          {children}
+          <span className="mx-10"></span>
+        </Marquee>
+      </div>
+      <div className="md:hidden text-xs inline-block">{children}</div>
     </div>
   );
 }
